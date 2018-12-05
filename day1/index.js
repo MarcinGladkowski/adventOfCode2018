ecae8ad759f683a0dfa9451e1eb74d78439bc3d4
Gladkowski <marcingladkowski@gmail.com>
2018-12-05 11:11:49 +0100

    result II part

--- day1/index.js
+++ day1/index.js
@@ -10,22 +10,44 @@ class CounterCalc {
             let inputParsed = /[0-9]+/g.exec(input);
             return [input[0], inputParsed[0]]
         });
+        this.sumResults = [this.sum];
+        this.twice = 0;
     }
 
-    countSum() {
+    countSum(findTwice = false) {
+
+        let isRepeat = true;
+
         for (var value of this.input) {
             const number = parseInt(value[1]);
             switch(value[0]) {
                 case '+':
                     this.sum += number;
+                    this.sumResults.push(this.sum);
                     break;
                 case '-':
                     this.sum -= number;
+                    this.sumResults.push(this.sum);
                     break;
             }
+            
+            if((this.sumResults.filter(item => item == this.sum).length == 2) && (findTwice === true)) {
+                findTwice = false;
+                return this.sum;                
+            }
         }
+
+        if(findTwice) {
+            this.countSum(true);
+        }
+
         return this.sum;
     }
 }
-// result
+// result I part
 const sum = new CounterCalc(result).countSum();
+console.log(sum);
+
+// result II part
+const sum = new CounterCalc(result).countSum(true);
+console.log(sum);
